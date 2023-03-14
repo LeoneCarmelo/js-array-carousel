7d36724eaaca0b5cec99f6f206bf36acb9dc7813
LeoneCarmelo <carmeloleone@outlook.it>
2023-03-14 17:05:44 +0100

    Add static arrows

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,19 @@ let images =  ''
 for(let i = 0; i < imgList.length; i++) {
     const imgName = imgList[i]
     images += `<img src="./assets/img/${imgName}" class="${i === activeImage ? 'visible' : ''}" >`
-    containerEl.innerHTML =  images
+    containerEl.innerHTML = images
 }
+// create arrow Up
+const arrowUp = document.createElement('button')
+arrowUp.style.top = '0'
+arrowUp.style.left = 'calc(100% / 2)'
+arrowUp.innerHTML = `<i class="fa-solid fa-angle-up"></i>`
+containerEl.insertAdjacentElement('afterbegin', arrowUp)
+// create arrow Down
+const arrowDown = document.createElement('button')
+arrowDown.style.bottom = '0'
+arrowDown.style.left = 'calc(100% / 2)'
+arrowDown.innerHTML = `<i class="fa-solid fa-angle-down"></i>`
+containerEl.insertAdjacentElement('beforeend', arrowDown)
 
 
