b0b9f3f6789d83df6590fd6a2d3cf5f14fd58882
LeoneCarmelo <carmeloleone@outlook.it>
2023-03-16 23:23:56 +0100

    Fix thumbnails

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,14 +22,16 @@ for(let i = 0; i < imgList.length; i++) {
 // create arrow Up
 const arrowUp = document.createElement('button')
 arrowUp.style.top = '0'
-arrowUp.style.left = 'calc(100% / 2)'
+arrowUp.style.right = '80px'
+arrowUp.style.zIndex = '1'
 arrowUp.innerHTML = `<i class="fa-solid fa-angle-up"></i>`
 containerEl.insertAdjacentElement('afterbegin', arrowUp)
 
 // create arrow Down
 const arrowDown = document.createElement('button')
 arrowDown.style.bottom = '0'
-arrowDown.style.left = 'calc(100% / 2)'
+arrowDown.style.right = '80px'
+arrowDown.style.zIndex = '1'
 arrowDown.innerHTML = `<i class="fa-solid fa-angle-down"></i>`
 containerEl.insertAdjacentElement('beforeend', arrowDown)
 
@@ -56,27 +58,12 @@ for (let i = 0; i < imgList.length; i++){
     thumbnails.insertAdjacentElement('beforeend', contTinyImage)
 }
 
-
-//listen for click up
-arrowUp.addEventListener('click', function(){     
-    //select all the image
-    const contImage = document.querySelectorAll('.container > img')
-    //select the current image
-    const currentImage = contImage[activeImage]
-    //remove visible class
-    currentImage.classList.remove('visible')
-    //Decrease activeImage variable
-    if (activeImage === 0) {
-        activeImage = imgList.length - 1
-    } else {
-        activeImage--
-    }
-    //create another variable for the next image
-    let nextImage = contImage[activeImage]
-    // add visible class to nextImage
-    nextImage.classList.add('visible')
-})
-
+  //select all the container of the tiny image
+  const allContImages = document.querySelectorAll('#cont-tiny-images > div')
+  //select the current container of the tiny image
+  let currentContImages = allContImages[activeImage]
+  currentContImages.classList.add('selected')
+  console.log(currentContImages)
 
 //listen for click down
 arrowDown.addEventListener('click', function() {
@@ -91,15 +78,48 @@ arrowDown.addEventListener('click', function() {
     //remove visible class
     currentImage.classList.remove('visible')
     //remove selected class
-    currentContImages.classList.add('selected')
+    currentContImages.classList.remove('selected')
     //increase activeImage variable
     if(activeImage === imgList.length - 1){
         activeImage = 0
     } else {
         activeImage++
     }
-       //create another variable for the next image
-       const nextImage = contImage[activeImage]
-       // add visible class to nextImage
-       nextImage.classList.add('visible')
+    //create another variable for the next image
+    const nextImage = contImage[activeImage]
+    // add visible class to nextImage
+    nextImage.classList.add('visible')
+    //create another variable for the next contImage
+    const nextContImage = allContImages[activeImage]
+    // add selected class to the nextContImage
+    nextContImage.classList.add('selected')
+})
+//listen for click up
+arrowUp.addEventListener('click', function(){     
+    //select all the image
+    const contImage = document.querySelectorAll('.container > img')
+    //select all the container of the tiny image
+    const allContImages = document.querySelectorAll('#cont-tiny-images > div')
+    //select the current image
+    const currentImage = contImage[activeImage]
+    //select the current container of the tiny image
+    const currentContImages = allContImages[activeImage]
+    //remove visible class
+    currentImage.classList.remove('visible')
+    //remove selected class
+    currentContImages.classList.remove('selected')
+    //Decrease activeImage variable
+    if (activeImage === 0) {
+        activeImage = imgList.length - 1
+    } else {
+        activeImage--
+    }
+    //create another variable for the next image
+    let nextImage = contImage[activeImage]
+    // add visible class to nextImage
+    nextImage.classList.add('visible')
+    //create another variable for the next contImage
+    const nextContImage = allContImages[activeImage]
+    // add selected class to the nextContImage
+    nextContImage.classList.add('selected')
 })
